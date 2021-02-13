87eff8c54bba16ffe6f2b97bd904bcd207b67fa4
Philipp Jardas <philipp@jardas.de>
2021-02-13 10:08:50 +0100

    update

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,77 @@
 const pug = require("pug");
+const { getDaysOfMonth, getDayOfWeek } = require("../lib/date-utils");
 
 exports.handler = async function (event) {
-  const { t } = event.queryStringParameters;
+  try {
+    const { t } = event.queryStringParameters;
+    const target = parseDate(t);
+    const now = createNow();
+    const months = getMonths(target, now);
 
-  return {
-    statusCode: 200,
-    headers: {
-      "content-type": "text/html;charset=utf-8",
-    },
-    body: template({ target: t, styles }),
-  };
+    return {
+      statusCode: 200,
+      headers: {
+        "content-type": "text/html;charset=utf-8",
+      },
+      body: template({ target, now, months, styles }),
+    };
+  } catch (error) {
+    return {
+      statusCode: error.statusCode || 500,
+      headers: {
+        "content-type": "text/plain;charset=utf-8",
+      },
+      body: error.message,
+    };
+  }
 };
+
+function parseDate(s) {
+  if (!t) throw statusError(400, "Target date missing");
+
+  const parts = s.split(/-/);
+  if (parts.length < 3) throw new statusError(400, `Invalid target: ${t}`);
+  return {
+    year: parseInt(parts[0], 10),
+    month: parseInt(parts[1], 10),
+    day: parseInt(parts[2], 10),
+  };
+}
+
+// FIXME timezone
+function createNow() {
+  const now = new Date();
+  return {
+    year: now.getFullYear(),
+    month: now.getMonth() + 1,
+    day: now.getDaysOfMonth(),
+  };
+}
+
+function getMonths(target, now) {
+  return Array.from(Array(target.month).keys())
+    .slice(now.month - 1)
+    .map((month) => {
+      const days = getDaysOfMonth(month);
+      const max = days[days.length - 1];
+      const padStart = getDayOfWeek(target.year, month, days[0]);
+      const weeks = [[...Array(padStart), ...days.slice(0, 7 - padStart)]];
+
+      for (let day = 7 - padStart; day < max; day += 7) {
+        const week = days.slice(day, day + 7);
+        if (week.length < 7) week.push(...Array(7 - week.length));
+        weeks.push(week);
+      }
+
+      return { month, weeks };
+    });
+}
+
+function statusError(statusCode, message) {
+  const error = new Error(message);
+  error.statusCode = statusCode;
+  return error;
+}
 
 const styles = `
 * {
@@ -69,6 +130,5 @@ style= styles
 title Countdown
 .page
   h1 Countdown
-  p Target:
-    strong= target
+  pre= JSON.stringify({target,now,months}, null, 2)
 `);
