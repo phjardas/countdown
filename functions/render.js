ec2e567a358337cc43e01938cf7056b71d2fe84e
Philipp Jardas <philipp@jardas.de>
2021-02-14 09:42:13 +0100

    update

---
+++ functions/render.js
@@ -0,0 +1,22 @@
+const { parseParams } = require('../lib/params');
+const renderers = require('../lib/render');
+
+exports.handler = async (event) => {
+  try {
+    const parts = event.path.replace(/^\/.netlify\/functions\/render\//, '').split('/');
+    const params = parseParams(parts[0]);
+    const type = parts.length > 1 ? parts[1] : 'index.html';
+    const renderer = renderers[type];
+    if (!renderer) return { statusCode: 404 };
+
+    return renderer(params);
+  } catch (error) {
+    return {
+      statusCode: error.statusCode || 500,
+      headers: {
+        'content-type': 'text/plain;charset=utf-8',
+      },
+      body: error.message,
+    };
+  }
+};
--- server.js
+++ server.js
@@ -1,24 +1,18 @@
 const http = require('http');
-const fs = require('fs');
 const path = require('path');
 const static = require('node-static');
 
 const port = 3000;
 
 const staticServer = new static.Server(path.resolve(__dirname, 'public'));
-
-const handlers = fs.readdirSync(path.resolve(__dirname, 'functions')).map((s) => ({
-  ...require(`./functions/${s}`),
-  prefix: `/${s.replace(/\.js$/, '')}/`,
-}));
+const render = require('./functions/render').handler;
+const renderPrefix = '/r/';
 
 const server = http.createServer(async (req, res) => {
   const url = new URL(req.url, `http://localhost:${port}`);
-  const handler = handlers.find((h) => url.pathname.startsWith(h.prefix));
-
-  if (handler) {
-    const event = { path: `/.netlify/functions${url.pathname}` };
-    const response = await handler.handler(event);
+  if (url.pathname.startsWith(renderPrefix)) {
+    const event = { path: `/.netlify/functions${url.pathname.replace(renderPrefix, '/render/')}` };
+    const response = await render(event);
     res.writeHead(response.statusCode, response.headers);
     res.end(response.body);
   } else {
