8e17e2e9849805bf755464f0e0985cb5b1690ba0
Philipp Jardas <philipp@jardas.de>
2021-02-15 08:13:10 +0100

    update

--- rollup.config.js
+++ rollup.config.js
@@ -3,18 +3,23 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import replace from '@rollup/plugin-replace';
 
+const plugins = [
+  nodeResolve(),
+  commonjs(),
+  replace({
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+  }),
+];
+
+if (process.env.NODE_ENV === 'production') {
+  plugins.push(terser());
+}
+
 export default ['sw.tpl.js', 'home.script.tpl.js'].map((input) => ({
   input: `src/render/${input}`,
   output: {
     dir: 'functions/render',
     format: 'cjs',
   },
-  plugins: [
-    nodeResolve(),
-    commonjs(),
-    replace({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-    }),
-    terser(),
-  ],
+  plugins,
 }));
