5e7fc90c2e95ff5d21590c6d811f1552dda2b776
Philipp Jardas <philipp@jardas.de>
2021-03-04 19:48:21 +0100

    update

--- src/App.svelte
+++ src/App.svelte
@@ -33,24 +33,26 @@
   $: url = s && `${location.origin.replace(/:(80|443)$/, '')}/r/${s}/`;
 </script>
 
-<h1>Countdown</h1>
-<form id="form">
-  <label for="l">Title:</label>
-  <input id="l" maxlength="50" required bind:value={title} />
-  <label for="t">Target date:</label>
-  <input type="date" id="t" required bind:value={date} />
-  <label for="i">Icon:</label>
-  <select id="i" required bind:value={icon}>
-    {#each icons as icon}
-      <option value={icon.id}>{icon.label}</option>
-    {/each}
-  </select>
-  <label for="p">Primary color:</label>
-  <input type="color" id="p" required bind:value={color} />
-</form>
-{#if url}
-  <p>
-    <a href={url} class="button" style={`--color: ${color}`}>Copy and share this link!</a>
-  </p>
-  <iframe id="preview" title="Preview" src={`${url}?p`} />
-{/if}
+<div class="page">
+  <h1>Countdown</h1>
+  <form id="form">
+    <label for="l">Title:</label>
+    <input id="l" maxlength="50" required bind:value={title} />
+    <label for="t">Target date:</label>
+    <input type="date" id="t" required bind:value={date} />
+    <label for="i">Icon:</label>
+    <select id="i" required bind:value={icon}>
+      {#each icons as icon}
+        <option value={icon.id}>{icon.label}</option>
+      {/each}
+    </select>
+    <label for="p">Primary color:</label>
+    <input type="color" id="p" required bind:value={color} />
+  </form>
+  {#if url}
+    <p>
+      <a href={url} class="button" style={`--color: ${color}`}>Copy and share this link!</a>
+    </p>
+    <iframe id="preview" title="Preview" src={`${url}?p`} />
+  {/if}
+</div>
